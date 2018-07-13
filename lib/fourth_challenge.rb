c4c90b7f88420c516d5599812f657b00a5d09fe6
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-13 01:10:43 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -11,7 +11,9 @@ def fourth_challenge
           attitude: "worried"
         }
         {
-          
+          name: "Mercutio", 
+          age: "18", 
+          attitude: "hot-headed"
         }
       ]
    }, 
