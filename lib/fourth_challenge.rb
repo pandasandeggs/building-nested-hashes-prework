b82cf71f229b1f45ae94a1e74333c4261eaa1215
Chantal Wallace <theteatreehouse@gmail.com>
2018-07-13 01:11:17 +0000

    automatically backed up by learn

--- lib/fourth_challenge.rb
+++ lib/fourth_challenge.rb
@@ -9,7 +9,7 @@ def fourth_challenge
           name: "Benvolio",
           age: "17",
           attitude: "worried"
-        }
+        },
         {
           name: "Mercutio", 
           age: "18", 
@@ -22,7 +22,13 @@ def fourth_challenge
       :matriarch => {name: "Lady Capulet", age: "51"},
       :heroine => {name: "Juliet", age: "15", status: "alive"},
       :heroine_friends => [
-        #your hashes here!
+        {
+          name: "Steven", 
+          age: "30", 
+          attitude: "confused"
+          
+        },
+          {name: "Nurse", age: "44", attitude: "worried"}
       ]
    }
 }
